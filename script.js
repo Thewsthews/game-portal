b4809311d813713f404d2744d1e35d188d1cabc8
Thewsthews <etiegnim@gmail.com>
2025-02-07 14:07:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const gameCards = document.querySelectorAll('.game-card');
     const modal = document.getElementById('game-modal');
     const modalDetails = document.getElementById('modal-details'); 
-    const closeModal = document.querySelector('.close-modal');
+   const closeModal = document.querySelector('.close-modal');
+   const searchInput = document.getElementById('game-search');
 
 
     function setTheme(theme) {
         if (theme === 'dark') {
             body.setAttribute('data-theme', 'dark');
             localStorage.setItem('theme', 'dark');
+            themeToggle.textContent = '🌞Light Mode';
         } else {
             body.removeAttribute('data-theme');
             localStorage.setItem('theme', 'light');
+            themeToggle.textContent = '🌙 Dark Mode';
         }
     }
 
     themeToggle.addEventListener('click', () => {
         const currentTheme = body.getAttribute('data-theme');
-        
+       
         if (currentTheme === 'dark') {
             setTheme('light');
         } else {
@@ -45,16 +48,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 <img src="${gameImage}" style="width: 100%; max-height: 300px; object-fit: cover;">
                 <p>${gameDesc}</p>
             `;
-            modal.style.display = 'block';
+            modal.classList.add('show-modal'); 
         });
     });
-    closeModal.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
-
+    closeModal.addEventListener('click', () => closeModal());
     window.addEventListener('click', (event) => {
         if (event.target === modal) {
-            modal.style.display = 'none';
+            modal.classList.remove('show-modal');
         }
-    });
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                modal.classList.remove('show-modal');
+            }
+        });
+        function closeModal() {
+            modal.classList.remove('show-modal');
+            setTimeout(()=> {modal.style.dsplay = 'none';}, 500);
+        }
+
+        searchInput.addEventListener('input', ()=>{
+            const query = searchInput.value.toLowerCase();
+            gameCards.forEach(carf => {
+                const gameName = card.getAttribute('data-name').toLowerCase();
+                card.style.display = gameName.includes(query) ? 'block' : 'none';
+        })
+        })
+    });  
 });
